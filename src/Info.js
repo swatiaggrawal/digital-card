20e05c063a28815fc7297d6c4796abcabd7ad2ef
Swati Aggrawal <swatiagg357@gmail.com>
2022-12-29 19:13:28 +0530

    update

--- src/Info.js
+++ src/Info.js
@@ -12,6 +12,20 @@ function Info(){
             <div className="website">
                 <a href='https://swatiaggrawal.github.io/WebPortfolio/' >WebPortfolio</a>
             </div>
+            <div className="email">
+            <button>E-mail</button>
+            </div>
+            <div className="linkdin">
+            <button>LinkdIn</button>
+            </div>
+            <div className="about">
+                <h1>About</h1>
+                <p>I am a final year BTECH CSE student learning about various technical concepts like Data Structures, Algorithms, Artificial Intelligence and Machine Learning.</p>
+            </div>
+            <div className='interest'>
+                <h1>Interests</h1>
+                <p>Food expert. Music scholar. Reader. Internet fanatic.Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            </div>
         </>
     )
 }
